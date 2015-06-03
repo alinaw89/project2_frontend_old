875c70d17dcb44dc53ecdb7573939d5352020cb5
alinaw89 <ali.naw89@gmail.com>
2015-06-03 14:36:18 -0400

    creates a basic layout with header

---
+++ js/compact.js
@@ -0,0 +1,23 @@
+// test run
+
+$('#new-prod-button').click(function(){
+  var cosmetic_product = {
+  name: $("#new-prod-name").val(),
+  brand: $("#new-prod-brand").val(),
+  color: $("#new-prod-color").val(),
+  price: parseFloat($("#new-prod-price").val()),
+  purchase_date: $("#new-prod-date").val(),
+  category: $("#new-prod-cat").val()};
+  // image: $("new-prod-image").val()};
+
+  $.ajax({
+    type: 'POST',
+    url: "http://localhost:3000/cosmetic_products",
+    data: {cosmetic_product: cosmetic_product}
+    }).done(function(){
+    alert ("success");
+    }).fail(function(){
+    alert ("failure");
+  });
+
+});
